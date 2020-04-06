fea13ca86e83a5bde1162e27e8160c976c7b3f61
Matias Ruiz <ruizmat@gmail.com>
2020-04-06 22:59:32 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   nombre: string = "Capitán América";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  personajes: string[] = ["Ironman", "Spiderman", "Thor", "Loki", "Groot"];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "B",
+    },
+  };
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve("llegaron los datos");
+    }, 4500);
+  });
 }
